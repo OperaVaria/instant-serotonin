a4cf3085ae140c177737f54f5696bbe070562398
OperaVaria <lcs_it@proton.me>
2024-01-29 11:33:26 +0100

    Streamlining

--- serotonin_flask_app.py
+++ serotonin_flask_app.py
@@ -16,28 +16,35 @@ import pickle
 app = Flask(__name__)
 
 
+# Function to get random post from pickled list:
+def load_from_pickle(file_path):
+    with open(file_path, "rb") as file:
+        sub_list = pickle.load(file)
+    post = random.choice(sub_list)
+    return post
+
+
+# Page building decorators:
 @app.route("/")
 def index():
-    """Set up index.html"""
-    return render_template("index.html", title_variable = "Instant Serotonin | Quick copium for everyone")
+    """Set up index page."""
+    return render_template("index.html", title_bar = "Instant Serotonin | Quick copium for everyone")
 
 @app.route("/capybara")
 def capybara():
-    """Set up capybara.html"""
-    with open("./instant-serotonin/data/capydata.p", "rb") as file:
-        sub_list = pickle.load(file)
-    post = random.choice(sub_list)
-    return render_template("capybara.html", title_variable = "Instant Capybara | Quick copium for everyone",
-                           capy_pic=post.url, capy_title=post.title, capy_uploader=post.author)
+    """Set up capybara page."""
+    post = load_from_pickle("./data/capydata.p")
+    return render_template("result.html", title_bar = "Instant Capybara | Quick copium for everyone",
+                           page_title="Instant Capybara", post_pic=post.url, post_title=post.title,
+                           post_location="r/capybara", post_uploader=post.author)
 
 @app.route("/manul")
 def manul():
-    """Set up manul.html"""
-    with open("./instant-serotonin/data/manuldata.p", "rb") as file:
-        sub_list = pickle.load(file)
-    post = random.choice(sub_list)
-    return render_template("manul.html", title_variable = "Instant Manul | Quick copium for everyone",
-                           manul_pic=post.url, manul_title=post.title, manul_uploader=post.author)
+    """Set up manul page."""
+    post = load_from_pickle("./data/manuldata.p")
+    return render_template("result.html", title_bar = "Instant Manul | Quick copium for everyone",
+                           page_title="Instant Manul", post_pic=post.url, post_title=post.title,
+                           post_location="r/PallasCats", post_uploader=post.author)
 
 # When run as main run on localhost, port 8080:
 if __name__ == '__main__':
