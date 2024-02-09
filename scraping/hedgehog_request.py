a26e86b7adb604858b63bb622365dc966a75d4b4
OperaVaria <lcs_it@proton.me>
2024-02-09 15:07:35 +0100

    More subreddits added

--- scraping/hedgehog_request.py
+++ scraping/hedgehog_request.py
@@ -2,4 +2,4 @@
 from reddit_scrape import reddit_collect
 
 # Scrape hedgehog subreddit:
-reddit_collect("Hedgehog", "hedgehog_request", "./instant-serotonin/data/hedgehdata.p")
+reddit_collect("Hedgehog+Hedgehogs+HedgehogsAreLiquid", "hedgehog_request", "./instant-serotonin/data/hedgehdata.p")
--- scraping/manul_request.py
+++ scraping/manul_request.py
@@ -2,4 +2,4 @@
 from reddit_scrape import reddit_collect
 
 # Scrape manul subreddit:
-reddit_collect("PallasCats", "manul_request", "./instant-serotonin/data/manuldata.p")
+reddit_collect("PallasCats+manuls", "manul_request", "./instant-serotonin/data/manuldata.p")
