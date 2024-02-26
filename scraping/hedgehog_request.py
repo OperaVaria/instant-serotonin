0075447c2aea75e7a9d31d31bae4b001d1be326d
OperaVaria <lcs_it@proton.me>
2024-02-26 02:28:57 +0100

    Cross-platform scripts

--- scraping/hedgehog_request.py
+++ scraping/hedgehog_request.py
@@ -4,8 +4,14 @@ hedgehog_request.py
 Part of the "Instant Serotonin" project by OperaVaria.
 """
 
+# Import Path class:
+from pathlib import Path
+
 # Import scraping function:
 from reddit_scrape import reddit_collect
 
+# Create an absolute path for output pickle file.
+output_file = Path(__file__).parents[1].resolve() / "data/hedgehog_data.p"
+
 # Scrape hedgehog subreddits:
-reddit_collect("Hedgehog+Hedgehogs+HedgehogsAreLiquid", "hedgehog_request", "./data/hedgehog_data.p")
+reddit_collect("Hedgehog+Hedgehogs+HedgehogsAreLiquid", "hedgehog_request", output_file)
--- scraping/manul_request.py
+++ scraping/manul_request.py
@@ -4,8 +4,14 @@ manul_request.py
 Part of the "Instant Serotonin" project by OperaVaria.
 """
 
+# Import Path class:
+from pathlib import Path
+
 # Import scraping function:
 from reddit_scrape import reddit_collect
 
+# Create an absolute path for output pickle file.
+output_file = Path(__file__).parents[1].resolve() / "data/manul_data.p"
+
 # Scrape manul subreddits:
-reddit_collect("PallasCats+manuls", "manul_request", "./data/manul_data.p")
+reddit_collect("PallasCats+manuls", "manul_request", output_file)
